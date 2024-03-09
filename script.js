e69c1d9b6092bb2e5943a259e1113348f8642334
CptCartoon <noobski87@gmail.com>
2024-03-09 23:43:02 +0100

    finished project

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const hamburgerMenu = document.querySelector('.icon-menu');
 const navLinks = document.querySelector('.nav-links');
 const navButton = document.querySelector('.btn-nav');
+const menuShadow = document.querySelector('.mobile-menu-shadow');
 
 function menu() {
     navLinks.classList.toggle('nav-mobile');
+    hamburgerMenu.classList.toggle('close-menu');
+
+    if(menuShadow.style.display === "block") {
+        menuShadow.style.display = "none";
+    } else {
+        menuShadow.style.display = "block";
+    }
+    
+
 }
 
 hamburgerMenu.addEventListener("click", menu)
